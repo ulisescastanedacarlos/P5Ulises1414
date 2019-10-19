52157b2f1d79cc18a233ba44fe3f0965e2f77c55
github_ulisescastanedacarlos <ulliisseess@outlook.com>
2019-10-18 21:07:30 -0500

    Initial commit

---
+++ app/src/main/java/ucc/itics/tesoem/edu/p5ulises1414/MainActivity.java
@@ -0,0 +1,21 @@
+package ucc.itics.tesoem.edu.p5ulises1414;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Mensaje (View v){
+        Toast.makeText(this,"Bienvenido a esta aplicacion", Toast.LENGTH_SHORT).show();
+
+    }
+}
